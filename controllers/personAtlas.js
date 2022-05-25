4366624954d9381893c3b08b0e950c41d70b264e
meriam <hamdaoui.meriam85@gmail.com>
2022-05-25 09:32:21 +0200

    routes & controllers

--- controllers/personAtlas.js
+++ controllers/personAtlas.js
@@ -9,7 +9,31 @@ exports.getAtlasAll = async (req, res) => {
   }
 };
 
-exports.postAtlas = async (req, res) => {
+//13. Create Many Records with model.create()
+const arrayOfPeople = [
+  {
+    name: "souad",
+    age: 43,
+    email: "souad@gmail.com",
+    favouritFoods: ["icecream", "strawberry", "pasta"],
+  },
+  {
+    name: "zohra",
+    age: 68,
+    email: "zohra@gmail.com",
+    favouritFoods: ["couscous", "green beans", "milk"],
+  },
+  {
+    name: "fatma",
+    age: 33,
+    email: "fatma@gmail.com",
+    favouritFoods: ["pasta", "cheese", "pizza"],
+  },
+];
+//i comment the ligne below to avoid the repetition inside my DB
+//exports.addMany = Person.create(arrayOfPeople);
+
+exports.addAtlas = async (req, res) => {
   try {
     const newPerson = await new Person(req.body);
     newPerson.save();
@@ -19,13 +43,45 @@ exports.postAtlas = async (req, res) => {
   }
 };
 
-exports.deleteAtlas = async (req, res) => {
+exports.getByName = async (req, res) => {
   try {
-    const { id } = req.params;
-    Person.findByIdAndDelete(id);
-    res.status(200).send("person deleted");
-  } catch (error) {
-    res.status(500).send("OUPS something went wrong DELETE");
+    let { name } = req.params;
+    const requestedPerson = await Person.findOne({ name: name });
+    res.status(200).send({ msg: "ur requested Person", requestedPerson });
+  } catch (err) {
+    console.error("get person name crashed => " + err);
+    //send an error msg
+    res.status(500).send("u could not get this person");
+  }
+};
+
+//16.Use model.findById() to Search Your Database By _id
+exports.getById = async (req, res) => {
+  try {
+    let { _id } = req.params;
+    const requestedIdPerson = await Person.findById({ _id });
+    res.status(200).send({ msg: "ur requested id Person", requestedIdPerson });
+  } catch (err) {
+    console.error("get person name crashed => " + err);
+    //send an error msg
+    res.status(500).send("u could not get this person");
+  }
+};
+
+//17.Perform Classic Updates by Running Find, Edit, then Save
+exports.classicUpdates = async (req, res) => {
+  try {
+    let { _id } = req.params;
+    let foodToAdd = "hamburger";
+    const updateData = await Person.findById({ _id });
+    // console.log(updateData);
+    updateData.favouritFoods.push(foodToAdd);
+    updateData.save();
+    res.status(200).send({ msg: "food added ", updateData });
+  } catch (err) {
+    console.error("add food crashed => " + err);
+    //send an error msg
+    res.status(500).send("u could not get this person");
   }
 };
 
@@ -36,5 +92,14 @@ exports.updateAtlas = async (req, res) => {
     res.status(200).send("person updated");
   } catch (error) {
     res.status(500).send("OUPS something went wrong UPDATE");
+  }
+};
+exports.deleteAtlas = async (req, res) => {
+  try {
+    const { id } = req.params;
+    Person.findByIdAndDelete(id);
+    res.status(200).send("person deleted");
+  } catch (error) {
+    res.status(500).send("OUPS something went wrong DELETE");
   }
 };
--- routes/personAtlas.js
+++ routes/personAtlas.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const {
   getAtlasAll,
-  postAtlas,
+  addAtlas,
   deleteAtlas,
   updateAtlas,
+  addMany,
+  getByName,
+  getById,
+  classicUpdates,
 } = require("../controllers/personAtlas");
 const atlasRouter = express.Router();
 
 atlasRouter.get("/", getAtlasAll);
-atlasRouter.post("/", postAtlas);
-atlasRouter.delete("/:id", deleteAtlas);
+atlasRouter.post("/addperson", addAtlas);
+// addMany();
+// atlasRouter.get("/:name", getByName);
+atlasRouter.get("/:_id", getById);
+atlasRouter.put("/:id", classicUpdates);
 atlasRouter.put("/:id", updateAtlas);
+atlasRouter.delete("/:id", deleteAtlas);
 
 module.exports = atlasRouter;
